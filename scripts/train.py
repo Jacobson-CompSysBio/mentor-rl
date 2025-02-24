ae586bf79edc2525101fbbfc1f6e7767ef510608
pikruse <pikruser@gmail.com>
2025-02-24 14:42:05 -0500

    starting skeleton code, new branch

---
+++ scripts/train.py
@@ -0,0 +1,15 @@
+# necessary imports
+import os, sys, glob, time
+import argparse
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+
+## CONFIG
+
+## LOAD DATA / MODEL
+
+## TRAIN LOOP
