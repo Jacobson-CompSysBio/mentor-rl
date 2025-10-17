d31ba1f0a35a576e7a2c9a7706af8c8d52843874
kpsmithjr <kpsmithjr@gmail.com>
2025-10-17 15:00:09 -0400

    Added the multiplex path as a required input to the distance QA generator. Upated unit tests.

--- scripts/distance_qa_generator.py
+++ scripts/distance_qa_generator.py
@@ -12,6 +12,10 @@ def parse_distance_qa_args():
                       type=str,
                       required=True,
                       help='Specify the file containing the distance matrix.')
+  parser.add_argument('--multiplex',
+                      type=str,
+                      required=True,
+                      help='Specify the flist used to generate the distance matrix.')
   parser.add_argument('--output_dir',
                       type=str,
                       default='./',
@@ -22,6 +26,11 @@ def parse_distance_qa_args():
                       help='Specify the output file name. If no value is provided "distance_qas.tsv" will be used.')
 
   return parser.parse_args()
+
+# Validate arguments
+def validate_arguments(args):
+  if not os.path.exists(args.multiplex):
+    raise ValueError('args.multiplex does not exist')
 
 # Read and validate distance matrix
 def read_distance_matrix(file_name: str) -> pd.DataFrame:
@@ -59,13 +68,13 @@ def read_distance_matrix(file_name: str) -> pd.DataFrame:
   raise ValueError("Distance matrix must be symmetric, or upper/lower-triangular with NaNs elsewhere")
 
 # Create Q/A pairs
-def write_qa_pairs(df: pd.DataFrame, dir: str, file_name: str) -> None:
-  os.makedirs(dir, exist_ok=True)
+def write_qa_pairs(df: pd.DataFrame, out_dir: str, out_file_name: str, multiplex_path) -> None:
+  os.makedirs(out_dir, exist_ok=True)
 
-  output_file = os.path.join(dir, file_name)
+  output_file = os.path.join(out_dir, out_file_name)
   
   with open(output_file, 'w') as fp:
-    fp.write('question\tlabel\n')
+    fp.write('question\tlabel\tdesc\n')
 
     labels = df.columns.to_list()
     N = len(labels)
@@ -74,13 +83,14 @@ def write_qa_pairs(df: pd.DataFrame, dir: str, file_name: str) -> None:
       for j in range(i+1,N):
         node2 = labels[j]
         dist = df.iloc[i,j]
-        fp.write( f'What is the topological distance between nodes {node1} and {node2}?\t{dist}\n')
+        fp.write( f'What is the topological distance between nodes {node1} and {node2}?\t{dist}\t{multiplex_path}\n')
 
 def main():
   args = parse_distance_qa_args()
+  validate_arguments(args)
 
   dist_df = read_distance_matrix(args.distance_matrix)
-  write_qa_pairs(dist_df, args.output_dir, args.outpur_file)
+  write_qa_pairs(dist_df, args.output_dir, args.outpur_file, args.multiplex)
 
 if __name__ == "__main__":
   main()
