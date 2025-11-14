f9b2e1e9b88fd9c21d52114874794e90aeea6827
Peter Kruse <peter.ingraham.kruse@gmail.com>
2025-11-14 16:31:28 -0500

    refactored to only have deepspeed files

--- scripts/debug.py
+++ scripts/debug.py
@@ -1,12 +1,7 @@
 ### SCRIPT FROM: https://cloud.google.com/ai-hypercomputer/docs/tutorials/fsdp-llama4 ###
-import os 
-
-# set HIP env vars before importing torch
-os.environ.setdefault("PYTORCH_HIP_ALLOC_CONF", "expandable_segments:True,max_split_size_mb:128")
-print("HIP alloc conf:", os.environ.get("PYTORCH_HIP_ALLOC_CONF"))  # sanity in logs
-
-import sys
+import os, sys
 import torch
+import argparse
 from pathlib import Path
 from datasets import load_dataset
 from peft import LoraConfig, PeftModel
@@ -42,8 +37,7 @@ slurm_args = argparse.Namespace(nnodes=nnodes, timeout=timeout)
 @dataclass
 class ScriptArguments:
     model_path: str = field(metadata={"help": "Hugging Face model ID from the Hub"})
-    dataset_path: str = field(metadata={"help": "Local dataset path"})
-    
+    dataset_path: str = field(default="/lustre/orion/syb111/proj-shared/Personal/krusepi/projects/llms/data/qa_pairs.json", metadata={"help": "Local dataset path"})
     run_inference_after_training: bool = field(default=False, metadata={"help": "Run sample inference on rank 0 after training"})
     dataset_subset_size: Optional[int] = field(default=None, metadata={"help": "Number of samples to use from the dataset for training. If None, uses the full dataset."})
 
@@ -58,9 +52,8 @@ class SftTrainingArguments(TrainingArguments):
     max_length: Optional[int] = field(default=2048, metadata={"help": "The maximum sequence length for SFTTrainer"})
     packing: Optional[bool] = field(default=False, metadata={"help": "Enable packing for SFTTrainer"})
     ddp_find_unused_parameters: Optional[bool] = field(default=True, metadata={"help": "When using FSDP activation checkpointing, this must be set to True"})
-    # num_train_epochs: Optional[float] = field(default=3.0, metadata={"help": "Total number of training epochs to perform (if not an integer, will perform the decimal part percents of the last epoch before stopping training)"})
 
-def build_formatting_func(tokenizer, train=True):
+def build_formatting_func(tokenizer):
     SYSTEM_PROMPT = (
         "You are a helpful biological chatbot. You will be given a biological question; "
         "return the correct answer."
@@ -70,34 +63,9 @@ def build_formatting_func(tokenizer, train=True):
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": example["question"]},
             {"role": "assistant", "content": example["answer"]},
-        ] if train else [
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": example["question"]},
         ]
         return tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
     return _fmt
-
-def infer(model, tokenizer, format, input):
-    results = []
-    with torch.no_grad():
-        for example in input:
-            formatted = format(example)
-            inputs = tokenizer(formatted, return_tensors="pt").to(model.device)
-            input_len = inputs["input_ids"].shape[1]
-            
-            output = model.generate(
-                **inputs,
-                max_new_tokens=50,
-                temperature=0.7,
-                top_p=0.9,
-                do_sample=True,
-                eos_token_id=tokenizer.eos_token_id
-            )
-
-            new_ids = output[0][input_len:]
-            text = tokenizer.decode(new_ids, skip_special_tokens=True)
-            results.append(text)
-    return results
 
 def main():
     # extract args from classes
@@ -106,31 +74,20 @@ def main():
 
     # make run name
     training_args.run_name = make_run_name(script_args, peft_args, training_args, slurm_args)
-
     training_args.optim = "adamw_torch_fused"
-
-    # set up FSDP
-    training_args.fsdp = "full_shard"
-    training_args.fsdp_config = {
-        "fsdp_auto_wrap_policy": "TRANSFORMER_BASED_WRAP",
-        "fsdp_state_dict_type": "FULL_STATE_DICT",
-        "fsdp_offload_params": False,
-        "fsdp_forward_prefetch": False,
-        # grad checkpointing through fsdp
-        "activation_checkpointing": True,
-        "activation_checkpointing_reentrant": False,
-    }
+    training_args.gradient_checkpointing = True
 
     # load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(script_args.model_path)
     formatting_func = build_formatting_func(tokenizer)
 
-    # load model     
+    # load model (attn is sdpa)
     model = AutoModelForCausalLM.from_pretrained(
         script_args.model_path,
         dtype=torch.bfloat16,
-        low_cpu_mem_usage=True,
+        attn_implementation="eager"
     )
+    model.gradient_checkpointing_enable()
     model.use_cache = False  # needed for gradient checkpointing
     model.config.use_cache = False
     model.config.output_attentions = False
@@ -143,7 +100,7 @@ def main():
         lora_dropout=peft_args.lora_dropout,
         bias="none",
         task_type="CAUSAL_LM",
-        target_modules=["q_proj", "v_proj", "k_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+        target_modules=["q_proj", "v_proj", "k_proj", "o_proj"],
     )
     
     # get rank, world size for distributed
@@ -171,13 +128,6 @@ def main():
     print("Initializing SFTTrainer...")
     training_args.report_to = ["wandb"]
 
-    # turn off gradient checkpointing
-    training_args.gradient_checkpointing = False
-    try:
-        model.gradient_checkpointing_disable()
-    except Exception:
-        pass
-
     trainer = SFTTrainer(
         model=model,
         args=training_args,
@@ -187,47 +137,8 @@ def main():
         processing_class=tokenizer,
     )
 
-    if rank == 0:
-        inference_set = dataset.select(range(20))
-        format_infer = build_formatting_func(tokenizer, train=False)
-        
-        print()
-        print("Peforming initial inference...")
-        outputs = infer(model, tokenizer, format_infer, inference_set)
-        print(check_accuracy(outputs, list(inference_set["answer"])))
-        print()
-        # for inp, out in zip(inference_set, outputs):
-        #     print(f"    Q: {inp['question']}")
-        #     print(f" True: {inp['answer']}")
-        #     print(f"Model: {out}")
-        #     print()
-
     trainer.train()
     trainer.save_model(training_args.output_dir)
-
-    if rank  == 0:
-        print()
-        print("Performing post-training inference...")
-        del trainer
-        del model
-        torch.cuda.empty_cache()
-
-        model = AutoModelForCausalLM.from_pretrained(
-            script_args.model_path,
-            torch_dtype=torch.bfloat16,
-        )
-        model = PeftModel.from_pretrained(model, training_args.output_dir)
-        # model = model.merge_and_unload()
-        model.eval()
-
-        outputs = infer(model, tokenizer, format_infer, inference_set)
-        print(check_accuracy(outputs, list(inference_set["answer"])))
-        print()
-        # for inp, out in zip(inference_set, outputs):
-        #     print(f"    Q: {inp['question']}")
-        #     print(f" True: {inp['answer']}")
-        #     print(f"Model: {out}")
-        #     print()
 
 if __name__ == "__main__":
     main()
